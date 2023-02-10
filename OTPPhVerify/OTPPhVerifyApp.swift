d2d584759e4d8678fa3f09052767eaabd5681339
GuruMahan <iosdeve2022@gmail.com>
2023-02-10 13:35:14 +0530

    Initial Commit

---
+++ OTPPhVerify/OTPPhVerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTPPhVerifyApp.swift
+//  OTPPhVerify
+//
+//  Created by Guru Mahan on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct OTPPhVerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
